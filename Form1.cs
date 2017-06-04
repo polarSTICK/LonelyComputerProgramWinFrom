69b87cdea8ae6529712bf0c61ebaea6abd000707
fearsomesole <makelamikael1@gmail.com>
2017-06-04 19:42:39 +0300

    New attepmt at LCP
    Since the console application proved to be less flexible and interactive
    for my purposes, I have created new project for maybe better results.

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,21 @@ namespace LonelyComputerProgramWinFrom
 {
 	public partial class Form1 : Form
 	{
+		[DllImport("Kernel32.dll")]
+		static extern Boolean AllocConsole();
+
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			if (!AllocConsole())
+				MessageBox.Show("Failed");
+			Console.WriteLine("test");
+			string input = Console.ReadLine();
+			MessageBox.Show(input);
 		}
 	}
 }
